405cc43a12b25dbaeb0e22b182934f4993a4b498
EzequielK-source <programacionezequielk@gmail.com>
2022-07-26 17:17:09 -0300

    (Feature) Create sequelize connection

---
+++ src/database/index.js
@@ -0,0 +1,6 @@
+const Sequelize = require('sequelize');
+const config = require('config')
+const {dialect,user,password,host,port,database} = config.get('Customer.dbConfig')
+
+
+module.exports = new Sequelize(`${dialect}://${user}:${password}@${host}:${port}/${database}`);
