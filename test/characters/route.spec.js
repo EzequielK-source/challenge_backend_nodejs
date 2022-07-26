90b5630442e8684e31f340bd3da003d3a8012fcd
EzequielK-source <programacionezequielk@gmail.com>
2022-07-26 15:15:58 -0300

    (Test) Add tests for Query params for GET method

--- test/characters/route.spec.js
+++ test/characters/route.spec.js
@@ -8,17 +8,48 @@ const { expect, request } = chai;
 //APP layer imports 
 const app = require('src/app');
 
-describe('App test', () => {
-    it('App is working up', (done) => {
-        request(app)
-            .get('/characters')
-            .end((err,res)=>{
-                if(err) done(err);
-
-                expect(res).to.have.status(200)
-                expect(res).to.be.json;
-                expect(res.body).to.have.property('characters')
-                done();
-            })
+describe('CharacterRouter test', () => {
+    describe('GET METHOD', () => {
+        it('GET method', (done) => {
+            request(app)
+                .get('/characters')
+                .end((err,res)=>{
+                    if(err) done(err);
+    
+                    expect(res).to.have.status(200)
+                    expect(res).to.be.json;
+                    expect(res.body).to.have.property('characters')
+                    done();
+                })
+        });
+        it('GET method filtered for age', (done) => {
+            const expecetedAge = 15;
+            request(app)
+                .get(`/characters?age=${expecetedAge}`)
+                .end((err,res)=>{
+                    if(err) done(err);
+    
+                    expect(res).to.have.status(200)
+                    expect(res).to.be.json;
+                    expect(res.body).to.have.property('characters')
+                    for (const object of res.body.characters) {
+                        expect(object.age).to.be.equal(expecetedAge)
+                    }
+                    done();
+                })
+        });
+        it('GET method filtered for movie', (done) => {
+            const movieId = 15;
+            request(app)
+                .get(`/characters?movie=${movieId}`)
+                .end((err,res)=>{
+                    if(err) done(err);
+    
+                    expect(res).to.have.status(200)
+                    expect(res).to.be.json;
+                    expect(res.body).to.have.property('characters')
+                    done();
+                })
+        });
     });
 });
