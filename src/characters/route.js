7e83890fbd042517fb9e245ba585bf934cb5ebf6
EzequielK-source <programacionezequielk@gmail.com>
2022-07-27 21:59:43 -0300

    (Feature) Implement FindAllCharacters functions to CharactersRouter

--- src/characters/route.js
+++ src/characters/route.js
@@ -2,10 +2,21 @@ const Router = require('express').Router;
 const CharactersRouter = Router();
 
 const FindAllCharacters = require('./controler/find_all_characters')
+const FindAllCharactersForAge = require('./controler/find_all_characters_for_age')
+const FindAllCharactersForName = require('./controler/find_all_characters_by_name')
 CharactersRouter.route('/')
     .get(async (req, res) => {
-        const characters = await FindAllCharacters(req.query);
-    
+        const {name, IDmovie, age} = req.query;
+        let characters;
+        if(name !== undefined){
+            characters = await FindAllCharactersForName(name)
+        }
+        if(age !== undefined){
+            characters = await FindAllCharactersForAge(age)
+        }
+        if(age === undefined && name == undefined){
+            characters = await FindAllCharacters(req.query);
+        }
         return res.json({ characters: characters}).status(200)
     })
 
