e2642011fd340055e3fd32206cbd2aa255232b0f
EzequielK-source <programacionezequielk@gmail.com>
2022-07-28 23:30:46 -0300

    (Test) Simplify Character GET method test

--- test/characters/routes/characters_get_route.spec.js
+++ test/characters/routes/characters_get_route.spec.js
@@ -11,39 +11,6 @@ const { expect, request } = chai;
 const app = require('src/app');
 const CreateCharacterCollection = require('test/utils/create_characters_collection')
 describe('GET METHOD', () => {
-    let characterOne;
-    let characterTwo;
-    let characterThree;
-    before(async () => {
-        //ID,image,name,age,weight,history
-        characterOne = {
-            image: 'optionA',
-            name: 'option A',
-            age: 9,
-            weight: 123,
-            history: "asdasdasd asdas"
-        }
-        characterTwo = {
-            image: 'optionB',
-            name: 'option B',
-            age: 90,
-            weight: 123,
-            history: "asdasdasd asdas"
-        }
-        characterThree = {
-            image: 'optionC',
-            name: 'marcelo',
-            age: 15,
-            weight: 123,
-            history: "asdasdasd asdas"
-        }
-        const characterCollection = [
-            characterOne,
-            characterTwo,
-            characterThree
-        ]
-        await CreateCharacterCollection(characterCollection)
-    });
     it('GET method', (done) => {
         request(app)
             .get('/characters')
@@ -65,9 +32,13 @@ describe('GET METHOD', () => {
                 expect(res).to.have.status(200)
                 expect(res).to.be.json;
                 expect(res.body).to.have.property('characters')
-                for (const object of res.body.characters) {
-                    expect(object.age).to.be.equal(expecetedAge)
+                const { characters } = res.body;
+                expect(characters).to.be.instanceOf(Array);
+
+                if (characters.length > 0) {
+                    expect(res.body.characters[0]).to.have.property('age').equal(expecetedAge)
                 }
+
                 done();
             })
     });
@@ -94,8 +65,11 @@ describe('GET METHOD', () => {
                 expect(res).to.have.status(200)
                 expect(res).to.be.json;
                 expect(res.body).to.have.property('characters')
-                for (const object of res.body.characters) {
-                    expect(object.name).to.contains(name)
+                const { characters } = res.body;
+                expect(characters).to.be.instanceOf(Array);
+
+                if (characters.length > 0) {
+                    expect(res.body.characters[0]).to.have.property('name').startsWith(name)
                 }
                 done();
             })
