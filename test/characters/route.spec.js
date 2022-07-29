c3021bb002fa53c4beea3d617b74078d270e78ec
EzequielK-source <programacionezequielk@gmail.com>
2022-07-28 22:04:59 -0300

    (Feature) Add POST method to CharactersRoute and implement CreateCharacter function

--- test/characters/route.spec.js
+++ test/characters/route.spec.js
@@ -1,7 +1,7 @@
 //chai sets and configs
 const chai = require('chai')
-    , chaiExclude = require('chai-exclude')
-    , chaiHttp = require('chai-http');
+    ,chaiExclude = require('chai-exclude')
+    ,chaiHttp = require('chai-http');
 chai.use(chaiHttp);
 chai.use(chaiExclude)
 
@@ -103,15 +103,29 @@ describe('CharacterRouter test', () => {
         });
     });
     it('POST METHOD', (done) => {
+        const sendRequest = {
+            image: 'path/to/image',
+            name: 'robert path',
+            age: 111,
+            weight: 11,
+            history: "HIPER BLA BLA"
+        }
         request(app)
             .post('/characters')
+            .send(sendRequest)
             .end((err, res) => {
                 if (err) done(err);
 
                 expect(res).to.have.status(201);
                 expect(res).to.be.json;
 
+                expect(res.body).to.have.property('character')
+                expect(res.body.character).excluding('ID')
+                    .to
+                    .be
+                    .deep
+                    .equal(sendRequest)
                 done();
-            })
+            });
     });
 });
